a2637253025d46b9ccb580f8620cdf797221d16b
Jiawei <liujiaweidreamer@gmail.com>
2020-07-19 19:18:25 +0800

    delete useless comments

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const db = require('./db.js');
 
 module.exports.add = async (todoTitle, status) => {
-  // 读文件取原来的 todoList
   const todoList = await db.read();
 
-  // 加入新的 todoTitle
   todoList.push({title: todoTitle, status: 'done' === status ? 'done' : 'undo'});
 
-  // 重新写入文件，直接覆盖
   await db.write(todoList)
 };
 
